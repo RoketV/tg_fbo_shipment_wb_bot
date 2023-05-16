4e7ef3750d29a2044cf7e04c00ceb8a360b37ca0
Vlados <vladislavmoroz96@yandex.ru>
2023-05-17 00:19:10 +0200

    feat: added validation for Initial Document With Sku

--- node/src/main/java/com/telegram_wb/util/DocumentValidatorImpl.java
+++ node/src/main/java/com/telegram_wb/util/DocumentValidatorImpl.java
@@ -2,16 +2,15 @@ package com.telegram_wb.util;
 
 import com.telegram_wb.enums.TypeOfDocument;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellAddress;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 import static com.telegram_wb.enums.TypeOfDocument.INITIAL_DOCUMENT_WITH_SKU;
@@ -22,6 +21,8 @@ import static com.telegram_wb.enums.TypeOfDocument.NOT_VALID_DOCUMENT;
 public class DocumentValidatorImpl implements DocumentValidator {
 
     private final Pattern pattern = Pattern.compile("^WB_\\d{10}$");
+
+    private final static Integer ACTIVE_DATA_CELLS = 3;
 
 
     public TypeOfDocument getDocumentType(byte[] byteArray) {
@@ -61,6 +62,36 @@ public class DocumentValidatorImpl implements DocumentValidator {
                 Objects.equals(cell2, "шк короба") &&
                 Objects.equals(cell3, "срок годности") &&
                 cell4.matches(pattern.pattern());
+    }
+
+    private boolean isWithData(Sheet sheet) {
+        //TO DO work with exceptions
+        Cell cell = Optional.ofNullable(getFirstActiveCell(sheet))
+                .orElseThrow(RuntimeException::new);
+        CellAddress activeCellAddress = cell.getAddress();
+        int firstRowNumber = activeCellAddress.getRow();
+        int lastRowNumber = activeCellAddress.getRow();
+        int firstActiveCell = activeCellAddress.getColumn();
+        if (sheet.getRow(firstRowNumber).getPhysicalNumberOfCells() > ACTIVE_DATA_CELLS) {
+            return false;
+        }
+
+
+
+        for (int i = firstRowNumber; i < lastRowNumber; i++) {
+
+        }
+    }
+
+    private Cell getFirstActiveCell(Sheet sheet) {
+        for (Row row : sheet) {
+            for (Cell cell : row) {
+                if (cell.getCellType() != CellType.BLANK) {
+                    return cell;
+                }
+            }
+        }
+        return null;
     }
 
     private boolean isNull(Workbook workbook) {
